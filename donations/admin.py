ed3de0ed61db646ca3ad27a8b9fe84d3006a1dda
Cajetan Rodrigues <caje731@gmail.com>
2016-03-20 18:46:21 +0530

    #102858072309662 Inline the Contact object

--- donations/admin.py
+++ donations/admin.py
@@ -1,12 +1,17 @@
 import django.contrib.admin as dca
 from donations.models import *
+
 # Register your models here.
 
 class AddressInline(dca.StackedInline):
 	model = Address
 
+class ContactInline(dca.StackedInline):
+	model = Contact
+
 class CaseDetailAdmin(dca.ModelAdmin):
 	inlines = [
+		ContactInline,
 		AddressInline,
 	]
 
